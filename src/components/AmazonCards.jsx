509e2df8336f3bfd79f131cae31c38ccfcf47789
Hemant Prajapati <hemantprajapati20500@gmail.com>
2022-05-24 23:52:39 +0530

    amazon and apple cards

---
+++ src/components/AmazonCards.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+const AmazonCards = (props) => {
+ 
+  return (
+    <div className='maindiv'style={{marginTop:props.margin_top,backgroundColor:props.color,borderRadius:'5px'}}>
+        <div className='imgndate'>
+          <h3>{props.Date}</h3>
+          <img src={props.image} alt="" />
+        </div>
+        <div>
+            <button>Case Study</button>
+        </div>
+        <h2>{props.fname}</h2>
+        <h2>{props.lname}</h2>
+        <div className='desnarrow'>
+        <h3>{props.sys}</h3>
+        <FontAwesomeIcon icon={faArrowRight} style={{position:'relative', top:'25px',right:'25px'}} />
+        </div>
+    </div>
+  )
+}
+
+export default AmazonCards
---
+++ src/components/Data.jsx
@@ -0,0 +1,23 @@
+const Data=[
+    {   
+        id:1,
+        color:'#FFA500',
+        Date:'28/10/2020',
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Amazon_icon.svg/2500px-Amazon_icon.svg.png",
+        fname:'Amazon Gift',
+        lname:'Pay',
+         sys:'Desktop-Mobile'
+    },
+    {   
+        id:2,
+        color:'#f5f5f5',
+        margin_top:'15px',
+        Date:'17 Sep 2020',
+        image:"https://media.idownloadblog.com/wp-content/uploads/2018/07/Apple-logo-black-and-white-429x500.png",
+        fname:'Apple Gift ',
+        lname:'Payment',
+         sys:'MacOS-Mobile'
+    }
+]
+
+export default Data
